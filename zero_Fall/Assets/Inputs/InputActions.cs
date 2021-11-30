19f1b2e9e120284d3068ca7f80147d2b2de733a3
mplouffe <matheuplouffe@gmail.com>
2021-11-29 20:36:31 -0600

    Updated to Unity 2020. Updated a bunch of packages.

--- zero_Fall/Assets/Inputs/InputActions.cs
+++ zero_Fall/Assets/Inputs/InputActions.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/InputActions.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Inputs/InputActions.inputactions'
 
 using System;
 using System.Collections;
